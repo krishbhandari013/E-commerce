2a3ca3e7261d3634bdd1c7e2ec9d8f4327e9a31e
Krish Bhandari <krishbhandari013@gmail.com>
2026-03-03 21:45:17 +0545

    small changes on backend folder

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -4,13 +4,13 @@ import productModel from '../models/productModel.js'
 const addProduct = async (req,res) =>{
     try{
 
-        const {name , description , price,category,subCatagory,sizes,bestseller} = req.body
+        const {name , description , price,category,subCategory,sizes,bestseller} = req.body
 
         const image1 = req.files.image1 && req.files.image1[0]
         const image2 = req.files.image2 && req.files.image2[0]
         const image3 = req.files.image3 && req.files.image3[0]
         const image4 = req.files.image4 && req.files.image4[0]
-        console.log(name , description , price,category,subCatagory,sizes,bestseller)
+        console.log(name , description , price,category,subCategory,sizes,bestseller)
         console.log(image1,image2,image3,image4)
 
         const images = [image1,image2,image3,image4].filter((item) => item !== undefined)
@@ -29,7 +29,7 @@ const addProduct = async (req,res) =>{
     description,
     price:Number(price),
     category,
-    subCatagory,  // match schema spelling
+    subCategory,  // match schema spelling
     sizes: JSON.parse(sizes),
     bestseller: bestseller === "true" ,
     image: imagesUrl,          // match schema name
